c33b7a0a61b376c9ec027fe9e0ae5dc9c5770e0f
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-12-11 16:44:29 +0200

    Don't push artifacts to remote locations and generate output

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,4 +5,8 @@ set -e -u
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
 cd java-buildpack-security-provider
-./mvnw -q -Dmaven.test.skip=true deploy
+./mvnw -Dmaven.test.skip=true package
+
+mkdir -p ../output
+cp $(ls target/java-buildpack-container-security-provider-*.jar | grep -v sources) ../output/
+echo $(cat target/maven-archiver/pom.properties  | grep version= | cut -d= -f2) > ../output/version
